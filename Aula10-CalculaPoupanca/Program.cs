9eb87748bfda1fb174468468a6ca5e33ae128aee
Lutiero <lutiero@gmail.com>
2023-05-17 15:24:05 -0400

    Aula 10 While

---
+++ Aula10-CalculaPoupanca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Aula 10 - Calcula valor da poupança");
+
+        double investimento = 1000.0;
+        double rendimento = 0.005;
+        int mes = 1;
+        //rendimento de 0,005 por mês
+        
+
+        while(mes <= 12)
+        {
+            investimento = investimento + investimento * rendimento;
+            Console.WriteLine("No mês " + mes + " vocé tem R$" + investimento);
+            mes++;
+        }
+
+
+        Console.WriteLine("Pressione Enter para encerrar...");
+        Console.ReadLine();
+
+    }
+}
+
