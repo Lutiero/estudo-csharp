2a219193a7486a7e0954fb2e47a0d904048adf6e
Lutiero <lutiero@gmail.com>
2023-05-17 10:43:04 -0400

    Aula 07 Condicionais

---
+++ Aula07-Condicionais/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+        int idade = 18;
+        int quantidadeAcompanhantesMaiorDeIdade = 1;
+
+        if (idade < 18 && quantidadeAcompanhantesMaiorDeIdade == 0)
+        {
+            Console.WriteLine("Menor de idade");
+            return;
+        }
+        else if (idade < 18 && quantidadeAcompanhantesMaiorDeIdade > 0)
+        {
+            Console.WriteLine("Pode entrar acompanhado");
+            return;
+        }
+
+        Console.WriteLine("Maior de idade");
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
