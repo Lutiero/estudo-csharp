80b7c739168ffac6d1061601e43f5daefbefc0e0
lutiero <lutiero@gmail.com>
2023-05-16 21:15:02 -0400

    iniitial commit

---
+++ Aula02-CriandoVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Aula 02 - Criando variáveis");
+        int idade;
+        
+        idade = 39;
+        Console.WriteLine(idade);
+        idade = idade - 5; 
+        Console.WriteLine(idade);
+        idade = 5 * 5 - 256;
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Pressione enter para encerrar");
+        Console.ReadLine();
+
+    }
+}
+
