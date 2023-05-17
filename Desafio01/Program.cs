b4eb783c50f4cae59c67c7c0874c3b5178f7261e
Lutiero <lutiero@gmail.com>
2023-05-17 14:34:42 -0400

    Desafio 01

---
+++ Desafio01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double salario = 3300.0;
+        double ir = 0.075;
+        int deducao = 142;
+        double valorPagoDeImposto = 0.0;
+
+        if (salario >= 1900.0 && salario <= 2800.0)
+        {
+            ir = 0.075;
+            deducao = 142;
+            valorPagoDeImposto = (salario * ir);
+        }
+        else if (salario >= 2800.01 && salario <= 3751.0)
+        {
+            ir = 0.15;
+            deducao = 350;
+            valorPagoDeImposto = (salario * ir);
+        }
+        else
+        {
+            ir = 0.225;
+            deducao = 636;
+            valorPagoDeImposto = (salario * ir);
+        }
+
+        Console.WriteLine("Dedução: " + deducao);
+        Console.WriteLine("IR: " + valorPagoDeImposto);
+    }
+}
