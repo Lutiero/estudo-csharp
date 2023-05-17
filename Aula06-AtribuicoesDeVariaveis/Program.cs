8ab2e4a6c329f893b85816041419ac5272d51b64
Lutiero <lutiero@gmail.com>
2023-05-17 10:18:45 -0400

    Aula 06 Atribuicoes de variaveis

---
+++ Aula06-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 6 - Atribuições de variáveis");
+
+        int idade = 6;
+        int idadeEduardo = idade;
+        Console.WriteLine("idadeEduardo: " + idadeEduardo);
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
