8a809f670dfc47f53c1ebf63ab806a034bf634ee
Lutiero <lutiero@gmail.com>
2023-05-17 08:59:10 -0400

    Aula  04 Conversao e outros tipos de variaveis

---
+++ Aula04-ConversaoEOutrosTipos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double salario;
+        salario = 3000.87;
+
+        int salarioInt = (int)salario;
+        Console.WriteLine(salarioInt);
+
+        long valorNaoSuportadoPorTipoInt = 200000000000000;
+
+        short tipoShort = 16000;
+
+        //menor precisão que o double
+        float tipoFloat = 15.156f;
+
+
+        Console.WriteLine("Pressione Enter para encerrar...");
+        Console.ReadLine();
+
+    }
+}
+
