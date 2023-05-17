c2ce3ea7277431fc42daf006608c72b6858f4b59
lutiero <lutiero@gmail.com>
2023-05-16 21:56:02 -0400

    Variaveis tipo  double

---
+++ Aula03-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double salario = 3000.16;
+        Console.WriteLine(salario);
+        double desconto = salario * 0.20;
+        Console.WriteLine(desconto);
+        salario = salario - desconto;
+        Console.WriteLine(salario);
+
+    }
+}
+
