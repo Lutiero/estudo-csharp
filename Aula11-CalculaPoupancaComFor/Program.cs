53da919c6e4b3ed4240545663ebb2094686cd084
lutiero <lutiero@gmail.com>
2023-05-17 19:00:02 -0400

    Aula 11 Laco for

---
+++ Aula11-CalculaPoupancaComFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Aula 11 - Calcula valor da poupança com laço for");
+
+        double investimento = 1000.0;
+        double rendimento = 1.005;
+
+        for (int mes = 1; mes <= 12; mes++)
+        {
+            investimento *= rendimento;
+            Console.WriteLine("O rendimento no mês " + mes + " foi de R$" + investimento);
+        }
+
+
+        Console.WriteLine("Pressione Enter para encerrar...");
+        Console.ReadLine();
+
+    }
+}
+
