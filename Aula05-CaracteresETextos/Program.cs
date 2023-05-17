23e055a72c2e905f5cb5c24215ee321314c6d17a
Lutiero <lutiero@gmail.com>
2023-05-17 09:32:08 -0400

    Aula 04 strings e char

---
+++ Aula05-CaracteresETextos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letra = 'a';
+        Console.WriteLine("char: " + letra);
+
+        // Tabela ASCII
+        letra = (char)(65 + 1);
+        Console.WriteLine("char(" + (int)letra + "): " + letra);
+
+        // Strings
+        string frase = "Um novo dia, uma nova regra, uma nova exceção.";        
+        Console.WriteLine("string: " + frase);
+
+        string cursos = @"Cursos disponíveis: 
+    - C# 
+    - Javascript 
+    - GO";
+        Console.WriteLine(cursos);
+
+        Console.WriteLine("");
+        Console.WriteLine("Pressione Enter para encerrar...");
+        Console.ReadLine();
+
+    }
+}
+
