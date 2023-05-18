2f550e43593c04a01d658c0fa7c533a29931c9a4
Lutiero <lutiero@gmail.com>
2023-05-18 14:47:23 -0400

    Utilzando o break

---
+++ Aula13-EncadeandoFor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 13 - Encadeando For");
+
+        for(int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            Console.WriteLine();
+            for(int contadorColunas = 0; contadorColunas < 10; contadorColunas++)
+            {
+                Console.Write('*');
+                if(contadorColunas >= contadorLinhas)
+                {
+                    break;
+                }
+            }
+        }
+
+        // Outra forma de fazer a mesma coisa
+
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            Console.WriteLine();
+            for (int contadorColunas = 0; contadorColunas <= contadorLinhas; contadorColunas++)
+            {
+                Console.Write('*');
+            }
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
